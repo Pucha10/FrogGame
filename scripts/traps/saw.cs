a21110df5a9bfafc4ba0dd00076d24e31f2f17af
Pucha10 <puszka.dawid02@gmail.com>
2024-02-03 00:02:39 +0100

    added saws and levers

--- scripts/traps/saw.cs
+++ scripts/traps/saw.cs
@@ -1,12 +1,19 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class saw : Area2D
 {
+	[Export]
+	public int timeOn;
+	[Export]
+	public int timeOff;
 	private AnimatedSprite2D animatedSprite2D;
 	private CollisionShape2D collisionShape2D;
 	private GameManager gameManager;
 	private Timer timer;
+	private List<lever> levers = new List<lever>();
 
 	public override void _Ready()
 	{
@@ -14,12 +21,34 @@ public partial class saw : Area2D
 		collisionShape2D = GetChild<CollisionShape2D>(1);
 		timer = GetChild<Timer>(2);
 		gameManager = GetNode<GameManager>("%GameManager");
-		timer.Start();
-	}
+		if (timeOn > 0)
+		{
+			timer.WaitTime = timeOn;
+			timer.Start();
+		}
+		foreach (Node child in GetChildren())
+		{
+			if (child is lever)
+			{
+				levers.Add((lever)child);
+			}
+		}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	}
+	public void SetOffAnimation()
 	{
+		int couter = 0;
+		foreach (lever lever in levers)
+		{
+			if (lever.isOpen)
+			{
+				couter++;
+			}
+		}
+		if (couter == levers.Count)
+		{
+			animatedSprite2D.Animation = "Off";
+		}
 	}
 	public void OnBodyEntered(Node2D body)
 	{
@@ -30,6 +59,15 @@ public partial class saw : Area2D
 	}
 	public void OnTimerTimeout()
 	{
-		animatedSprite2D.Animation = "On";
+		if (animatedSprite2D.Animation == "On")
+		{
+			animatedSprite2D.Animation = "Off";
+			timer.WaitTime = timeOff;
+		}
+		else
+		{
+			animatedSprite2D.Animation = "On";
+			timer.WaitTime = timeOn;
+		}
 	}
 }
