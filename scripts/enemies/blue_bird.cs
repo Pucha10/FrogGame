22af8e0b5ba610a633f228f9c2892aac204b3fbd
Pucha10 <puszka.dawid02@gmail.com>
2024-02-06 15:17:18 +0100

    added enemies to level 3

--- scripts/enemies/blue_bird.cs
+++ scripts/enemies/blue_bird.cs
@@ -5,11 +5,12 @@ public partial class blue_bird : RigidBody2D
 {
 	private GameManager gameManager;
 	private AnimatedSprite2D animatedSprite2D;
+	[Export]
 	private float speed = 300;
 	public override void _Ready()
 	{
 		gameManager = GetNode<GameManager>("%GameManager");
-		LinearVelocity = new Vector2(-speed, 0);
+		ConstantForce = new Vector2(-speed, 0);
 		animatedSprite2D = GetChild<AnimatedSprite2D>(0);
 
 	}
@@ -18,12 +19,14 @@ public partial class blue_bird : RigidBody2D
 		animatedSprite2D.Scale = new Vector2(-animatedSprite2D.Scale.X, animatedSprite2D.Scale.Y);
 		if (speed > 0)
 		{
-			LinearVelocity = new Vector2(speed, 0);
+			LinearVelocity = new Vector2(speed / 4, 0);
+			ConstantForce = new Vector2(speed, 0);
 			speed = -speed;
 		}
 		else
 		{
-			LinearVelocity = new Vector2(speed, 0);
+			LinearVelocity = new Vector2(speed / 4, 0);
+			ConstantForce = new Vector2(speed, 0);
 			speed = -speed;
 		}
 	}
@@ -33,7 +36,7 @@ public partial class blue_bird : RigidBody2D
 		{
 			animatedSprite2D.Animation = "Hit";
 			body.Velocity = new Vector2(body.Velocity.X, -900);
-			LinearVelocity = new Vector2(LinearVelocity.X, 300);
+			LinearVelocity = new Vector2(speed, 0);
 			gameManager.AddCherriesPoints(3);
 		}
 	}
@@ -41,7 +44,8 @@ public partial class blue_bird : RigidBody2D
 	{
 		if (body.Name == "CharacterBody2D" && animatedSprite2D.Animation != "Hit")
 		{
-			LinearVelocity = new Vector2(speed, 0);
+			LinearVelocity = new Vector2(speed / 4, 0);
+			ConstantForce = new Vector2(speed, 0);
 			animatedSprite2D.Scale = new Vector2(-animatedSprite2D.Scale.X, animatedSprite2D.Scale.Y);
 			speed = -speed;
 			gameManager.DoDamage();
